d8b446f0b34468f250d4c287031941f9ec9b9c52
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-02-22 20:53:13 +0400

    ByteStream fix.

--- ByteStream/index.js
+++ ByteStream/index.js
@@ -330,7 +330,7 @@ class ByteStream {
     header.writeUInt16BE(this.id, 0)
     header.writeUIntBE(this.buffer.length, 2, 3)
     header.writeUInt16BE(this.version, 5)
-    this.client.write(Buffer.concat([header, this.buffer]))
+    this.client.write(Buffer.concat([header, this.buffer, Buffer.from([0xFF, 0xFF, 0x0, 0x0, 0x0, 0x0, 0x0])]))
     this.client.log(`Packet ${this.id} (${this.constructor.name}) was sent.`)
   }
 }

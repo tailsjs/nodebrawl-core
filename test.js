a30f3e3984a056f4f4b3db326b9b3e30e92233ac
tailsjs <sepo.r@mail.ru>
2024-09-27 09:34:58 +0400

    [3.0-nightly13] Update!

--- test.js
+++
@@ -1,4 +0,0 @@
-const LogicMersenneTwisterRandom = require("./Titan/Random/LogicMersenneTwisterRandom")
-const seed = 1488
-const random = new LogicMersenneTwisterRandom(seed)
-console.log(random.rand(98))
